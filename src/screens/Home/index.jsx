06c060a13e365cc627f549d3282ba81409c9f822
Victormattos564 <victormaria@aluno.senai.br>
2024-03-13 08:34:56 -0300

    Adiciona componente Title e estilos para Home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import styles from './styles'
 export default function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   )
