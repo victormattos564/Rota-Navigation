3702dbe1e28572785e42965a985581467c2e2b36
Victormattos564 <victormaria@aluno.senai.br>
2024-03-13 09:17:03 -0300

    Adiciona telas de perfil e categoria

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,13 +1,17 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import Category from "../screens/Category";
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Category" component={Category} />
     </Tab.Navigator>
   );
 };
